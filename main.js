8ec2a09f078637537072bcb45e4c222b425ad831
Irene Spezzamonte <irenespezzamonte@MacBook-Pro-2.local>
2018-04-25 19:19:52 -0400

    data&flipping

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-$(document).ready(function(){
-    $(".filter").on("change", function(e){
-      e.preventDefault();
+$(document).ready(function () {
+	$(".filter").on("change", function (e) {
+		e.preventDefault();
 
-    var filter =  $(this).val();
+		var filter = $(this).val();
 
-    $(".item.active").removeClass("active");
+		$(".item.active").removeClass("active");
+		$(".disease.active").removeClass("active");
 
-    $("." + filter).addClass("active");
+		$("." + filter).addClass("active");
+	});
 
-    });
+	$(".disease").on("click", function (e) {
+		var disease = $(this);
+		if (disease.hasClass("active")) {
+			disease.removeClass("active");
+		} else {
+			disease.addClass("active");
+		}
+	});
 });
